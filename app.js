127f62c36f5c1ef13e106768a849efcaf52ee606
Fatyma Ferdous <125547915+FatymaFerdous@users.noreply.github.com>
2023-05-16 15:05:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function getNum(num) {
+    document.getElementById('result').value += num;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function backNum() {
+    let res = document.getElementById('result').value;
+    document.getElementById('result').value = document.getElementById('result').value.slice(0, res.length - 1)
+}
+
+function getResult() {
+    document.getElementById('result').value = eval(document.getElementById('result').value)
+}
+
+
+
